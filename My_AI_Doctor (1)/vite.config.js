992b826cfb636b35753f3e4e9e5260aa6e7b32f6
Murali-mmk <murali.mantha@viprata.com>
2025-02-01 19:26:34 +0530

    added a config plugin

--- My_AI_Doctor (1)/vite.config.js
+++ My_AI_Doctor (1)/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),  svgr(),],
   build: {
     chunkSizeWarningLimit: 60000,
     
@@ -11,4 +11,5 @@ export default defineConfig({
   server: {
     port: 3000, // Set the port to 3000
   },
+  
 })
