98c2d3ae60f12d60fb068b480cb55c8f4da38817
Murali-mmk <murali.mantha@viprata.com>
2025-02-03 09:57:44 +0530

    test

--- My_AI_Doctor (1)/vite.config.js
+++ My_AI_Doctor (1)/vite.config.js
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import svgr from '@svgr/rollup'; 
+// import svgr from '@svgr/rollup'; 
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),  svgr(),],
+  plugins: [react(), ],
   build: {
     chunkSizeWarningLimit: 60000,
     
